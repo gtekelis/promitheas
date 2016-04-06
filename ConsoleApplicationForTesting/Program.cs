d7a18de0c4187fea04d2282b054e9e0a967d7432
George Tekelis <tekelisg@gmail.com>
2016-04-06 17:01:18 +0300

    ok

--- ConsoleApplicationForTesting/Program.cs
+++ ConsoleApplicationForTesting/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PromitheasDomain.Classes;
+using PromitheasDomain.DataModel;
 
 namespace ConsoleApplicationForTesting
 {
@@ -10,6 +12,36 @@ namespace ConsoleApplicationForTesting
     {
         static void Main(string[] args)
         {
+            //InsertShop();
+            GetAllShops();
+        }
+
+        private static void InsertShop()
+        {
+            var shop = new Shop
+            {
+                Title = "Tattoo & Art By George",
+                Address = "Εσπερίδων 2",
+                ZipCode = "18757",
+                City = "Κερατσίνι",
+                State = "Αττική"
+            };
+
+            using (var context = new PromitheasContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Shops.Add(shop);
+                context.SaveChanges();
+            }
+        }
+
+        private static List<Shop> GetAllShops()
+        {
+            using (var context = new PromitheasContext())
+            {
+                var shops = context.Shops.ToList();
+                return shops;
+            }
         }
     }
 }
