30aa59cc12d502f43e73272a761912f1f6e365fb
George Tekelis <tekelisg@gmail.com>
2016-04-07 14:01:19 +0300

    ok

---
+++ AdministrationMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdministrationMVC.Startup))]
+namespace AdministrationMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
